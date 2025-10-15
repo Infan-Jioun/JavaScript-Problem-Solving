7489a36fd863b1382e7e8ae3cb1811d2a97a6e02
infan11 <infanjiounrahman20606@gmail.com>
2025-10-15 11:06:28 +0600

    Solving-1 added loop

--- Basic-JavaScript/solving-1.js
+++ Basic-JavaScript/solving-1.js
@@ -1,1 +1,15 @@
-console.log("hello")
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (1 % 3 === 0) {
+            console.log("Fizz")
+        } else if (1 % 5 === 0){
+         console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+
+    }
+}
+fizzBuzz(15)
