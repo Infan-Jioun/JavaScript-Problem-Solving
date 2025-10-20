4ed6ba65e7706e7a40abb67d05684330d1e2a76f
infan11 <infanjiounrahman20606@gmail.com>
2025-10-20 10:14:09 +0600

    update Solving-1

--- Basic-JavaScript/solving-1.js
+++ Basic-JavaScript/solving-1.js
@@ -1,15 +1,3 @@
-function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-        if (i % 3 === 0 && i % 5 === 0) {
-            console.log('FizzBuzz');
-        } else if (1 % 3 === 0) {
-            console.log("Fizz")
-        } else if (1 % 5 === 0){
-         console.log("Buzz");
-        } else {
-            console.log(i);
-        }
-
-    }
+function solveMeFirst(a, b) {
+    return a + b;
 }
-fizzBuzz(15)
