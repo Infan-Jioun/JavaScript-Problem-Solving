378567f77d78c7696357696bdaab1318f73e120d
infan11 <infanjiounrahman20606@gmail.com>
2025-10-20 10:30:21 +0600

    added solving-5

---
+++ Basic-JavaScript/solving-5.js
@@ -0,0 +1,11 @@
+function diagonalDifference(arr) {
+   let n = arr.length;
+   let primary = 0;
+   let secondary = 0;
+   for(let i = 0; i < n; i++) {
+       primary += arr[i][i];
+       secondary += arr[i][n - i - 1];
+   }
+    return Math.abs(primary - secondary);
+}
+
